63f8d38db8139675a710c666dcdb36d9d4c84006
gabriel-vitebo <vitebo@outlook.com>
2023-04-04 20:30:06 -0300

    creating table with knex

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,12 +1,11 @@
+const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
 
 class UsersController {
   async create(request, response) {
     const { name, email, password, isAdm } = request.body
-    if (!name) {
-      throw new AppError("hello")
-    }
-    response.json({ name, email, password, isAdm })
+    await knex("users").insert({ name, email, password, isAdm })
+    response.json()
   }
 }
 
