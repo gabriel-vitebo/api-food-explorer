affd346c2ced957eda8c70061223a0ea696f6c11
gabriel-vitebo <vitebo@outlook.com>
2023-06-26 14:40:11 -0300

    creating end-point to favorite food

--- src/controllers/FavoritesController.js
+++ src/controllers/FavoritesController.js
@@ -2,7 +2,18 @@ const knex = require("../database/knex");
 const { v4: uuidv4 } = require("uuid");
 
 class FavoritesController {
-  async adding(request, response) {}
+  async adding(request, response) {
+    const { food_id } = request.body;
+    const user_id = request.user.id;
+
+    await knex("favorites").insert({
+      id: uuidv4(),
+      food_id,
+      user_id,
+    });
+
+    return response.json();
+  }
 }
 
 module.exports = FavoritesController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,17 @@
-const { Router } = require("express")
+const { Router } = require("express");
 
-const usersRoutes = require("./users.routes")
-const foodsRoutes = require("./foods.routes")
-const categoriesRoutes = require("./categories.routes")
-const sessionsRoutes = require("./sessions.routes")
+const usersRoutes = require("./users.routes");
+const foodsRoutes = require("./foods.routes");
+const categoriesRoutes = require("./categories.routes");
+const sessionsRoutes = require("./sessions.routes");
+const favoritesRoutes = require("./favorites.routes");
 
-const routes = Router()
+const routes = Router();
 
-routes.use("/users", usersRoutes)
-routes.use("/sessions", sessionsRoutes)
-routes.use("/foods", foodsRoutes)
-routes.use("/categories", categoriesRoutes)
+routes.use("/users", usersRoutes);
+routes.use("/sessions", sessionsRoutes);
+routes.use("/foods", foodsRoutes);
+routes.use("/categories", categoriesRoutes);
+routes.use("/favorites", favoritesRoutes);
 
-module.exports = routes
+module.exports = routes;
